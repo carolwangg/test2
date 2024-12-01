942ab2385961b61a95f7e151530c3c4188825916
Carol Wang <carolzjwang@gmail.com>
2024-12-01 18:46:02 -0500

    added requirements.txt, vercel.json, and refactored venv into flask_app folder + edited app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -6,6 +6,3 @@ app = Flask(__name__, static_url_path='/static')
 @app.route('/')
 def home():
     return render_template('main.html')
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
